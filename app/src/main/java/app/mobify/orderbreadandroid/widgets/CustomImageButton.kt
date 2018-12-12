771d5b8be6a6449b1d3c082fa9979113883c7f68
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-12-12 12:16:25 -0200

    breadking app because of custom component fixed

--- app/src/main/java/app/mobify/orderbreadandroid/widgets/CustomImageButton.kt
+++ app/src/main/java/app/mobify/orderbreadandroid/widgets/CustomImageButton.kt
@@ -6,14 +6,22 @@ import android.widget.RelativeLayout
 import app.mobify.orderbreadandroid.R
 import kotlinx.android.synthetic.main.custom_image_button.view.*
 
+// https://medium.com/@elye.project/building-custom-component-with-kotlin-fc082678b080
+class CustomImageButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : RelativeLayout(context, attrs, defStyleAttr, defStyleRes) {
 
-class CustomImageButton(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) :
-    RelativeLayout(context, attrs, defStyleAttr) {
     init {
         inflate(context, R.layout.custom_image_button, this)
 
         // Load the styled attributes and set their properties
-        val typedArray = context?.obtainStyledAttributes(attrs, R.styleable.CustomImageButton, defStyleAttr, 0)
+        val typedArray = context.obtainStyledAttributes(
+            attrs,
+            R.styleable.CustomImageButton, defStyleAttr, 0
+        )
 
         val src = typedArray?.getDrawable(R.styleable.CustomImageButton_cib_src)
         val text = typedArray?.getText(R.styleable.CustomImageButton_cib_text)
