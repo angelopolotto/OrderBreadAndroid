b1d561148ed92aa808d9a2de8ad55d48251d097e
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-12-13 15:38:15 -0200

    messages changed

--- app/src/main/java/app/mobify/orderbreadandroid/utils/memoryStore/MemoryStore.kt
+++ app/src/main/java/app/mobify/orderbreadandroid/utils/memoryStore/MemoryStore.kt
@@ -3,7 +3,7 @@ package app.mobify.orderbreadandroid.utils.memoryStore
 import app.mobify.orderbreadandroid.api.models.Bread
 
 class MemoryStore: MemoryStoreContract {
-    override var maxPerItem: Int = 2
-    override var maxItemsCart: Int = 2
+    override var maxPerItem: Int = 2 //todo get from backend
+    override var maxItemsCart: Int = 2 //todo get from backend
     override lateinit var bread: Bread
 }
